5d92794e6827d798675a47ccda76b9e4af2fc80f
noam76 <31979739+noam76@users.noreply.github.com>
2026-09-02 21:35:27 +0300

    Create EnvironmentConfig.ts

---
+++ src/config/EnvironmentConfig.ts
@@ -0,0 +1,27 @@
+/**
+ * Wichy
+ * Environment Configuration Definition
+ *
+ * Based on:
+ * - TECH_STACK.md
+ * - ARCHITECTURE.md
+ */
+
+export interface Environme*tConfig {
+  isDevelopment: boolean*
+
+  isProduction: boolean;
+
+  isTe*ting: boolean;
+
+  application*ataPath: string;
+
+  cache*ath* string;
+
+  logsPath: string;
+
+  p*ofilesPath: string;
+
+  projectsPat*: string;
+}
+*
