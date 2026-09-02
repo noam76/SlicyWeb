397d5cc15184a76f2ebd527887531cf02b1c9031
noam76 <31979739+noam76@users.noreply.github.com>
2026-09-02 18:26:35 +0300

    Create Filament.ts

---
+++ src/types/Filament.ts
@@ -0,0 +1,30 @@
+/**
+ * Wichy
+ * Filament Type Definition
+ *
+ * Based on:
+ * - DATA_SCHEMA.md
+ * - FILAMENT_SETTINGS_SPEC.md
+ */
+
+export interface Filament {
+  id: string;
+
+  brand: string;
+
+  name: string;
+
+  material: string;
+
+  color: string;
+
+  diameter: number;
+
+  density: number;
+
+  recommendedProfile: object;
+
+  manufacturerSettings: object;
+
+  pricePerKg: number;
+}
